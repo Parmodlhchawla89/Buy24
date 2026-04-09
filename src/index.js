04e4aee564a9b18249aab208dfe37b5a3651b33f
Parmodh Chawla <parmodhc@nedbank.co.za>
2026-04-09 10:27:00 +0530

    updated google id

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId="12896094412-b36pke2nmhelidoc61j0fhcou9hkafnm.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="149316151642-ib5khu89ksu4ulnp5crqv5dj7kdh4tce.apps.googleusercontent.com">
         <React.StrictMode>
             <App />
         </React.StrictMode>
